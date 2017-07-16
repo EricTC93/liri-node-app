467fdbaaa39ba7703687acfe99b4a582d72573e9
Eric Tyler Cooper <tyler_726@live.com>
2017-07-16 07:23:38 -0700

    Delete key.js

--- key.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: 'tmsarbioFF9JqCojS4kZNNd5a',
-  consumer_secret: 'o0AL4TWvtZnFd01qhVuJKywpqaztLC2f0pB672ZQF1wgKiYRyC',
-  access_token_key: '884017669225062401-7H3w4a7TemYRUEAjF0WE0CyFZzn6gTp',
-  access_token_secret: 'f2UP1CuT0LahMOfABAf2EhtkrM2roNiFyZ1IvBEJ8wh19',
-}
